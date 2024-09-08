91915daf3b0449bccc4f14f1e1471c3a346eac7c
ephraim <github@beltran.cc>
2024-09-07 20:53:45 -0400

    plugins(linting and formatting): load eslint and prettier by default

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,8 +19,11 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "plugins" },
   },
   defaults = {
