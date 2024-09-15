44535b84e84fe57fc09831dd4a2cafbcce1caf27
ephraim <github@beltran.cc>
2024-09-14 22:50:33 -0400

    feat(plugin): install NeoTest plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "plugins" },
   },
